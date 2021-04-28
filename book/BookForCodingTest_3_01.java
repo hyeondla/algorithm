50e355ea46867fedc0948f44107272839a2ad4f9
hyeondla <hyeondla@gmail.com>
2021-04-28 14:38:04 +0900

    2021-04-28

---
+++ book/BookForCodingTest_3_01.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class BookForCodingTest_3_01 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		int money = sc.nextInt();
+		int count = 0;
+		int[] coin = { 500, 100, 50, 10 };
+
+		for (int i = 0; i < coin.length; i++) {
+			count += money / coin[i];
+			money %= coin[i];
+		}
+		System.out.println(count);
+	}
+
+}
---
+++ book/BookForCodingTest_3_02.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class BookForCodingTest_3_02 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int m = sc.nextInt();
+		int k = sc.nextInt();
+		int[] arr = new int[n];
+		for (int i = 0; i < arr.length; i++) {
+			arr[i] = sc.nextInt();
+		}
+
+		Arrays.sort(arr);
+
+		int max1 = arr[n - 1];
+		int max2 = arr[n - 2];
+
+		int count = m / (k + 1);
+
+		int sum = (m - count) * max1 + count * max2;
+		System.out.println(sum);
+
+	}
+
+}
