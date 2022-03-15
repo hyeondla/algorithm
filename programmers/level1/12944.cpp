a5a2e2136f3cfbad64ba1acb37b960504d187a18
hyeondla <79393928+hyeondla@users.noreply.github.com>
2022-03-15 17:14:19 +0900

    2022-03-15 평균 구하기

---
+++ programmers/level1/12944.cpp
@@ -0,0 +1,16 @@
+// 평균 구하기
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+double solution(vector<int> arr) {
+    
+    double sum = 0;
+    for(int i=0; i<arr.size(); i++) {
+        sum += arr[i];
+    }
+    double answer = sum / arr.size();
+    return answer;
+}
